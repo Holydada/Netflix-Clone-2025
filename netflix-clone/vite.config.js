7648388a0c1a30e82e68ac9e5d91d0e931d6574e
dawit abera <jesuschristmydestiny@gmail.com>
2025-05-23 23:17:56 +0300

    Update package.json and vite.config.js for Netlify deployment as StreamSphere

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Matches Netlify's publish directory
+  },
+  base: "/Netflix-Clone-2025/", // Matches homepage for GitHub Pages
+});

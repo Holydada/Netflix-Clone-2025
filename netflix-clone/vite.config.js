f7ea79b0f0de743d330e3d3f8fd76e6367a8fa95
dawit abera <jesuschristmydestiny@gmail.com>
2025-05-23 23:32:17 +0300

    Initial push for Netflix Clone project

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // Matches Netlify's publish directory
-  },
-  base: "/Netflix-Clone-2025/", // Matches homepage for GitHub Pages
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+    },
+    base: isProduction ? "/Netflix-Clone-2025/" : "/",
+  };
 });

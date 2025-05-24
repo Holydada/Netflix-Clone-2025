704f019fc3aed253672a0dffdf73ba66584e24f1
dawit abera <jesuschristmydestiny@gmail.com>
2025-05-24 21:19:30 +0300

    Fix import path for row component

--- netflix-clone/src/Components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Row from '../Row/Row'
-import requests from '../../../utils/Requests';
+import requests from '../../../utils/requests.jsx';
 const RowList = () => {
   return (
     <div>
